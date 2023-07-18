dd7b7e0678d744b122f48aba76876e2fa2805fbb
HeshanKarunaratne <heshan947102@gmail.com>
2023-07-18 08:19:33 +0530

    GET,POST endpoints

--- express/index.js
+++ express/index.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: "courses1" },
+    { id: 2, name: "courses2" },
+    { id: 3, name: "courses3" }
+];
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3, 4]);
+    res.send(courses);
 });
 
-app.get('/api/courses/:year/:month', (req, res) => {
-    // res.send(req.query);
-    res.send(req.params);
+app.get('/api/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send("The course with the given ID not found");
+
+    res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
 });
 
 const port = process.env.PORT || 3000;
